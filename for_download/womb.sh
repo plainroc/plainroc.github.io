45d37dffd9dd470457055b7634f0ec0474241441
CaiLiangpeng <plainroc@qq.com>
2015-07-21 17:38:18 +0800

    womb can download dna

--- for_download/womb.sh
+++ for_download/womb.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 echo hello
+mkdir -p seed
+curl -L -o seed/dna.uft8.txt https://gist.githubusercontent.com/plainroc/d3ad8119718468573a80/raw/a9026a6cc34057f03e79a4602f5a2f8129a4aa8e/app_seed_DNA
