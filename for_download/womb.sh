69677ef5dc739916c2d8c2358bbd2f2a54088eda
CaiLiangpeng <plainroc@qq.com>
2015-07-30 09:18:22 +0800

    refine download scrpit to make sure only the first line is parsed

--- for_download/womb.sh
+++ for_download/womb.sh
@@ -3,7 +3,7 @@ echo this is womb downloader
 mkdir -p seed
 #curl -L -o seed/womb.py https://gist.githubusercontent.com/plainroc/cf6fd282432bcaa8ec8e/raw/0aba17d1083777228b66c972014a5b3419bce66c/mac_womb.py
 curl https://api.github.com/gists/cf6fd282432bcaa8ec8e > seed/womb.py.gist 
-raw_url=$(cat seed/womb.py.gist | grep "raw_url" | cut -d '"' -f 4)
+raw_url=$(cat seed/womb.py.gist | grep "raw_url" | sed -n "1p" | cut -d '"' -f 4)
 echo $raw_url
 curl -L -o seed/womb.py $raw_url 
 python seed/womb.py
